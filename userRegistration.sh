195120d05fd76d2acc20853ae8951a3874a295a6
Archana-Bhamare <archusonar2@gmail.com>
2020-10-15 06:47:36 +0530

    Added : First Name

---
+++ userRegistration.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Enter your First Name : " userName
+regex='^[A-Z]{1}[a-z]{2,}$'
+echo $userName | grep -P -q $regex
+if [ $? -eq 0 ]
+then
+	echo "Valid Username"
+else
+	echo "Invalid Username"
+fi
