a463a1b6a6d29f24b5582433e425ab03fd27ded2
Archana-Bhamare <archusonar2@gmail.com>
2020-10-15 07:09:15 +0530

    Added : Email-Id

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+#First Name
+read -p "Enter your First Name : " userName
+regex='^[A-Z]{1}[a-z]{2,}$'
+echo $userName | grep -P -q $regex
+if [ $? -eq 0 ]
+then
+	echo "Valid Username"
+else
+	echo "Invalid Username"
+fi
+#Last Name
+read -p "Enter your Last Name : " userName
+regex='^[A-Z]{1}[a-z]{2,}$'
+echo $userName | grep -P -q $regex
+if [ $? -eq 0 ]
+then
+        echo "Valid Username"
+else
+        echo "Invalid Username"
+fi
+#Email-ID
 read -p "Enter your Email ID : " email
 regex="^[a-zA-Z0-9]+([.+-_][a-z-A-Z0-9]+)*@[a-zA-Z0-9]+.[a-z]{2,3}([.][a-z]{2})*$"
 echo $email | grep -P -q $regex
