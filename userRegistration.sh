af4f04165017c72d82fdd8d541a7b9d00bd002c2
Archana-Bhamare <archusonar2@gmail.com>
2020-10-15 06:58:04 +0530

    Added : Last Name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p "Enter your Last Name : " userName
+read -p "Enter your First Name : " userName
 regex='^[A-Z]{1}[a-z]{2,}$'
 echo $userName | grep -P -q $regex
 if [ $? -eq 0 ]
@@ -8,4 +8,14 @@ then
 	echo "Valid Username"
 else
 	echo "Invalid Username"
+fi
+
+read -p "Enter your Last Name : " userName
+regex='^[A-Z]{1}[a-z]{2,}$'
+echo $userName | grep -P -q $regex
+if [ $? -eq 0 ]
+then
+        echo "Valid Username"
+else
+        echo "Invalid Username"
 fi
