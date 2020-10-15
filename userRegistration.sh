84d990ef311a11540992c6af58817dab14294f91
Archana-Bhamare <archusonar2@gmail.com>
2020-10-15 22:07:37 +0530

    Added : Password_NumericNumber

--- userRegistration.sh
+++ userRegistration.sh
@@ -41,7 +41,7 @@ fi
 
 #Password
 read -p "Enter password : " password
-regex="^[a-zA-Z0-9]{8,}$"
+regex="^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$"
 if [[ $password =~ $regex ]]
 then
 	echo "Valid Password"
